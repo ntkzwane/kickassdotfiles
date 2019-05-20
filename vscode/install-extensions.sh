e9b1b18cb25d65e6c5368f04be4267545684de9b
Ntokozo Zwane <ntkzwane@gmail.com>
2019-05-20 23:31:49 +0200

    Updated zsh config
    Add my vscode extensions

---
+++ vscode/install-extensions.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+code --install-extension eamodio.gitlens &&
+code --install-extension eg2.tslint &&
+code --install-extension esbenp.prettier-vscode &&
+code --install-extension James-Yu.latex-workshop &&
+code --install-extension quicktype.quicktype &&
+code --install-extension shardulm94.trailing-spaces &&
+code --install-extension vscodevim.vim &&
+code --install-extension vsmobile.vscode-react-native &&
+code --install-extension wayou.vscode-todo-highlight
