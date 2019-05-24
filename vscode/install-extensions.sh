788ebb84275784c17858a3dd1ffeef45363ac6de
Ntokozo Zwane <ntkzwane@gmail.com>
2019-05-24 15:02:12 +0100

    Updated install script for vscode

--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -1,10 +1,3 @@
 #!/bin/zsh
-code --install-extension eamodio.gitlens &&
-code --install-extension eg2.tslint &&
-code --install-extension esbenp.prettier-vscode &&
-code --install-extension James-Yu.latex-workshop &&
-code --install-extension quicktype.quicktype &&
-code --install-extension shardulm94.trailing-spaces &&
-code --install-extension vscodevim.vim &&
-code --install-extension vsmobile.vscode-react-native &&
-code --install-extension wayou.vscode-todo-highlight
+# skip the first line in extensions (it's just a comment)
+tail -n +2 extensions | xargs -L 1 code --install-extension
